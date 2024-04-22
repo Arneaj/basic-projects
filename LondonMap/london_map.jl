04488266178c082b14efac59a1a8df0b9d361c8c
Arneaj <arnaud.rollandmail@gmail.com>
2024-04-22 11:42:38 +0200

    hihihi

--- LondonMap/london_map.jl
+++ LondonMap/london_map.jl
@@ -1,8 +1,26 @@
-using GLMakie, ImageCore
+using GLMakie, Images
+using LinearAlgebra
 
+function main()
 
+    map = rotr90( load("data/rgb.png") )
 
+    dim = size(map)
 
+    lat = Base._linspace(51.4083, 51.5775, dim[2])
+    lon = Base._linspace(-0.4012, 0.0929, dim[1])
+
+    fig = Figure()
+    ax = GLMakie.Axis( fig[1,1], aspect=DataAspect() )
+
+    image!( ax, map )
+
+    display( fig )
+
+    return
+end
+
+main()
 
 
 
