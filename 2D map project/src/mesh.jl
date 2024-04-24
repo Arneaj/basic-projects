5a71f52cafe8f7250dde13728d557a356b3f7804
Arneaj <arnaud.rollandmail@gmail.com>
2024-04-24 14:30:56 +0200

    implemented London map and started working on a 3D planet version of the 2D map

--- 2D map project/src/mesh.jl
+++ 2D map project/src/mesh.jl
@@ -3,39 +3,72 @@ using .Threads
 using LinearAlgebra
 using GLMakie, ImageCore, Colors
 
-function mesh_ball()
-    Theta = 0:0.1:2*pi
-    Phi = 0:0.1:pi
-    R = 1
+function mesh_plane( resolution )
+    points = [ Point3f(i%resolution, i÷resolution, 0) * 2/(resolution-1) - Point3f(1,1,0)  for i in 0:resolution^2-1 ]
 
-    #X = [ R*cos(theta) for theta in Theta ]
-    #Y = [ R*sin(theta) for theta in Theta ]
+    triangles = zeros( Int32, 2*(resolution-1)^2, 3 )
 
-    X = [ R*cos(theta)*cos(phi) for theta in Theta, phi in Phi ]
-    Y = [ R*sin(theta)*cos(phi) for theta in Theta, phi in Phi ]
-    Z = [ R*sin(phi) for theta in Theta, phi in Phi ]
-
-    points = []
-
-    for i in 1:length(Theta), j in 1:length(Phi)
-        push!( points, GLMakie.Point3f( X[i,j], Y[i,j], Z[i,j] ) )
+    for x in 1:resolution-1
+        for y in 1:resolution-1
+            triangles[2*(x+(y-1)*(resolution-1))-1, :] = [(y-1)*resolution+x, y*resolution+x, (y-1)*resolution+x+1]
+            triangles[2*(x+(y-1)*(resolution-1)), :] = [y*resolution+x+1, (y-1)*resolution+x+1, y*resolution+x]
+        end
     end
 
-    return points
+    return points, triangles
 end
 
-function plane()
-    
+function mesh_cube( resolution )
+    up, down, left, right, front, back = 1:6
+    faces = [ mesh_plane( resolution ) for _ in 1:6 ]
+
+    R90y = [0 0 1; 0 1 0; -1 0 0]
+    R90z = [1 0 0; 0 0 -1; 0 1 0]
+
+    faces[left][1] = -R90y * faces[left][1]
+    faces[right][1] = R90y * faces[right][1]
+    faces[front][1] = R90z * faces[front][1]
+    faces[back][1] = -R90z * faces[back][1]
+
+    faces[up][1] .+= Point3f( 0,0,0.5 )
+    faces[down][1] .-= Point3f( 0,0,0.5 )
+    faces[left][1] .-= Point3f( 0.5,0,0 )
+    faces[right][1] .+= Point3f( 0.5,0,0 )
+    faces[front][1] .-= Point3f( 0,0.5,0 )
+    faces[back][1] .+= Point3f( 0,0.5,0 )
+
+    for i in 1:6
+        faces[i][2] .+= (i-1)*(resolution)^2
+    end
+
+    points::Vector{Point3f} = []
+    triangles = zeros( Int32, 6*2*(resolution-1)^2, 3 )
+
+    for i in 1:6
+        push!( points, faces[i][1]... )
+        triangles[1+(i-1)*2*(resolution-1)^2:i*2*(resolution-1)^2, :] = faces[i][2]
+    end
+
+    return points, triangles
 end
 
 function main()
+    resolution = 3
+    points, triangles = mesh_cube( resolution )
 
     fig = Figure()
-    ax = Axis( fig[1,1], aspect = DataAspect() )
-    mesh!( ax, mesh_ball(), color=:gray, shading = NoShading )
+    ax = Axis3( fig[1,1] ) #LScene( fig[1,1] )
+
+    mesh!( ax, points, triangles, color=:red, shading=NoShading )
+
+    scatter!( ax, points, color=:blue) #, overdraw=true )
+
+    for i in 1:6*2*(resolution-1)^2
+        p::Vector{Point3f} = [points[triangles[i,:]]..., points[triangles[i,1]] ]
+        lines!( ax, p, color=:blue) #, overdraw=true ) 
+    end
 
     display( fig )
-
     return
 end
 
--- LondonMap/london_map.jl
+++ LondonMap/london_map.jl
@@ -1,19 +1,56 @@
 using GLMakie, Images
 using LinearAlgebra
+using DelimitedFiles
 
 function main()
+    lat_max = 51.5775
+    lat_min = 51.4083
+    lon_max = 0.0929
+    lon_min = -0.4012
 
     map = rotr90( load("data/rgb.png") )
 
-    dim = size(map)
+    dim = size( map )
+    #dlon = ( lon_max - lon_min ) / dim[1]
+    #dlat = ( lat_max - lat_min ) / dim[2]
 
-    lat = Base._linspace(51.4083, 51.5775, dim[2])
-    lon = Base._linspace(-0.4012, 0.0929, dim[1])
+    #Lon = lon_min:dlon:lon_max
+    #Lat = lat_min:dlat:lat_max
+
+    function get_ipos( lon, lat )
+        ilon::Int32 = round( dim[1] * (lon - lon_min) / (lon_max - lon_min) )
+        ilat::Int32 = round( dim[2] * (lat - lat_min) / (lat_max - lat_min) )
+        return Point2f(ilon, ilat)
+    end
+
+    #################### setting positions
+
+    ICL_ipos = get_ipos( -0.1769, 51.4983 )
+
+    Circle_line_ipos::Vector{Point2f} = []
+    District_line_ipos::Vector{Point2f} = []
+    Piccadilly_line_ipos::Vector{Point2f} = []
+
+    stations = readdlm( "data/stations.csv", ',' )
+    nb_rows = size(stations)[1]
+
+    for k in 2:nb_rows
+        #if stations[k, 9] > 1 end
+        if occursin( "Circle", stations[k, 6] ) push!( Circle_line_ipos, get_ipos(stations[k, 9:10]...) ) end
+        if occursin( "District", stations[k, 6] ) push!( District_line_ipos, get_ipos(stations[k, 9:10]...) ) end
+        if occursin( "Piccadilly", stations[k, 6] ) push!( Piccadilly_line_ipos, get_ipos(stations[k, 9:10]...) ) end
+    end
+
+    #################### plot
 
     fig = Figure()
     ax = GLMakie.Axis( fig[1,1], aspect=DataAspect() )
 
     image!( ax, map )
+    scatter!( ax, ICL_ipos, markersize=15, strokewidth=1.0 )
+    scatter!( ax, Circle_line_ipos, markersize=10, strokewidth=0.5, alpha = 0.5 )
+    scatter!( ax, District_line_ipos, markersize=10, strokewidth=0.5, alpha = 0.5 )
+    scatter!( ax, Piccadilly_line_ipos, markersize=10, strokewidth=0.5, alpha = 0.5 )
 
     display( fig )
 
